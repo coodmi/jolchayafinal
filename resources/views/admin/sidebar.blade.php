d0d35196737ee4914e210b4999bae89a78ee9ff3
Asif Mollik <115562335+asifmollik12@users.noreply.github.com>
2026-04-03 00:41:32 +0600

    remove: প্লট রেজিস্ট্রেশন from admin sidebar

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -95,17 +95,6 @@
             <span>বুকিং</span>
         </div>
 
-        <div class="nav-item" data-tab="registrations" onclick="showTab('registrations')">
-            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                <polyline points="14 2 14 8 20 8"></polyline>
-                <line x1="16" y1="13" x2="8" y2="13"></line>
-                <line x1="16" y1="17" x2="8" y2="17"></line>
-                <polyline points="10 9 9 9 8 9"></polyline>
-            </svg>
-            <span>প্লট রেজিস্ট্রেশন</span>
-        </div>
-
         <div class="nav-item" data-tab="news" onclick="showTab('news')">
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10l4 4v10a2 2 0 01-2 2z"></path>
